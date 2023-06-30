3d62b02e01e576bb314211aafd17fa34351c784d
SEONGEUN0415 <110333484+SEONGEUN0415@users.noreply.github.com>
2023-06-30 13:05:17 +0900

    Improve Rendering
    using transform

--- browser_chap03/main.js
+++ browser_chap03/main.js
@@ -3,16 +3,19 @@ const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const coordinates = document.querySelector('.coordinates');
 
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width/2;
+const targetHalfHeight = targetRect.height/2;
+
 document.addEventListener("mousemove", (event) => {
     const x = event.clientX;
     const y = event.clientY;
     
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    coordinates.style.left = `${x}px`;
-    coordinates.style.top = `${y}px`;
+    
+    vertical.style.transform = `translate(${x}px, 0)`;
+    horizontal.style.transform = `translate(0, ${y}px)`;
+    target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
+    coordinates.style.transform = `translate(${x}px, ${y}px)`;
     coordinates.innerHTML = `${x}px, ${y}px`;
 });
 
